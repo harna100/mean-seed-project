61fc8da1f4ce1b9d06bb8854c67cc5934340829e
Paul Harnack <harna100@mail.chapman.edu>
2017-09-19 15:28:20 -0700

    Fixes CORS

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function(req,res,next){
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Origin', 'POST, GET, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
 
